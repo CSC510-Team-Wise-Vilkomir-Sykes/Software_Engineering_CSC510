aac1536da878c48cdd20a78ba7e8c5f0828928cb
Katerina <92109549+Kii4ka@users.noreply.github.com>
2024-08-22 19:18:31 -0400

    Update numbers_test.py

--- numbers_test.py
+++ numbers_test.py
@@ -1,15 +1,6 @@
 import pytest
 from number_translator import int_to_roman 
 
-def test_pass():
-    assert int_to_roman(1994) == "MCMXCIV"
-
-def test_pass2():
-    assert int_to_roman(58) == "LVIII"
-
-def test_pass3():
-    assert int_to_roman(10) == "X"
-
 def test_failure():
     assert int_to_roman(2024) == "MMXX" 
 
@@ -18,3 +9,12 @@ def test_failure2():
 
 def test_failure3():
     assert int_to_roman(9) == "X"
+
+def test_pass():
+    assert int_to_roman(1994) == "MCMXCIV"
+
+def test_pass2():
+    assert int_to_roman(58) == "LVIII"
+
+def test_pass3():
+    assert int_to_roman(10) == "X"
