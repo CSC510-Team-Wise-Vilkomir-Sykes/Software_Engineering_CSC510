4e4dc1795bfd89bbec1f42394cc7a8df96023551
Jackson Sykes <jrsykes2@ncsu.edu>
2024-09-02 11:57:08 -0400

    Remove failing tests from HW1

--- hw1/numbers_test.py
+++ hw1/numbers_test.py
@@ -1,14 +1,14 @@
 import pytest
 from number_translator import int_to_roman 
 
-def test_failure():
-    assert int_to_roman(2024) == "MMXX" 
-
-def test_failure2():
-    assert int_to_roman(57) == "LVI"
-
-def test_failure3():
-    assert int_to_roman(9) == "X"
+# def test_failure():
+#     assert int_to_roman(2024) == "MMXX"
+#
+# def test_failure2():
+#     assert int_to_roman(57) == "LVI"
+#
+# def test_failure3():
+#     assert int_to_roman(9) == "X"
 
 def test_pass():
     assert int_to_roman(1994) == "MCMXCIV"
