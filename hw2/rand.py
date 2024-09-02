d8134fa03d7044b49d87591a9c5e43d298af0d3f
Jackson Sykes <jrsykes2@ncsu.edu>
2024-09-02 12:34:44 -0400

    Fix pylint issues with rand.py

--- hw2/rand.py
+++ hw2/rand.py
@@ -1,8 +1,17 @@
+"""Imports subprocess to run the shuf command."""
 import subprocess
 
 def random_array(arr):
+    """ Randomizes an array.
+
+    Argument:
+    arr -- array to randomize
+
+    Returns:
+    A randomized version of the given array
+    """
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
+    for i, _ in enumerate(arr):
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
