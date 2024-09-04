88fd1d90433ee0049d4252ca3b48f1cb37e48a66
Katerina <92109549+Kii4ka@users.noreply.github.com>
2024-09-04 18:45:22 -0400

    Update test_cases.py

--- hw2/test_cases.py
+++ hw2/test_cases.py
@@ -1,6 +1,6 @@
 """Module provides test cases for group project"""
 from hw2_debugging import merge_sort
- 
+
 def test_unsorted():
     """Test Case 1: Unsorted array"""
     arr = [5, 3, 2, 1, 8, 4, 10, 11, 9, 6, 111, 7]
@@ -12,7 +12,7 @@ def test_sorted():
     arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 111]
     sorted_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 111]
     assert merge_sort(arr) == sorted_arr
- 
+
 def test_empty():
     """Test Case 3: Empty array"""
     arr = []
