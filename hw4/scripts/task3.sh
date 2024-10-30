1309ef79ec89e26376fb5a00f193e2d5bc5eaa01
Jackson Sykes <jrsykes2@ncsu.edu>
2024-10-29 20:53:18 -0400

    Fix task3.sh for regrade

--- hw4/scripts/task3.sh
+++ hw4/scripts/task3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd .. && gawk -F',' ' BEGIN { OFS = "," } NR > 1 { if ($3 == 2 && substr($13, 1, 1) == "S") { print $0 } }' titanic.csv | sed 's/male/M/g; s/female/F/g' | gawk -F',' '$7 != "" {totalAges += $7; numAges++ } END { print totalAges/numAges; }'
+cd .. && gawk -F',' ' BEGIN { OFS = "," } NR > 1 { if ($3 == 2 && substr($13, 1, 1) == "S") { print $0 } }' titanic.csv && gawk -F',' ' BEGIN { OFS = "," } NR > 1 { if ($3 == 2 && substr($13, 1, 1) == "S") { print $0 } }' titanic.csv | sed 's/male/M/g; s/female/F/g' && gawk -F',' ' BEGIN { OFS = "," } NR > 1 { if ($3 == 2 && substr($13, 1, 1) == "S") { print $0 } }' titanic.csv | sed 's/male/M/g; s/female/F/g' | gawk -F',' '$7 != "" {totalAges += $7; numAges++ } END { print totalAges/numAges; }'
